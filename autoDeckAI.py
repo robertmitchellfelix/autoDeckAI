3e20f1f57febb4e546a902a85550a6cc6f702f10
Robert Felix <132026224+robertmitchellfelix@users.noreply.github.com>
2023-05-11 10:58:07 -0700

    autoDeckAI.py

--- autoDeckAI.py
+++ autoDeckAI.py
@@ -6,16 +6,12 @@
 import json
 import openai
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-def getFlashcards(apiKey, question, temperature):
-    if question.lower() == "exit":
-        return None
-
-    openai.api_key = apiKey #https://platform.openai.com/account/billing/overview
+def getFlashcards(apiKey, question, temperature, filename):
+    openai.api_key = apiKey
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -25,12 +21,32 @@ def getFlashcards(apiKey, question, temperature):
             ],
         temperature=temperature
         )
-    saveToJson(completion.choices[0].message.content)
+    saveToJson(completion.choices[0].message.content, filename)
     return completion.choices[0].message.content
 
-def saveToJson(content):
+def saveToJson(content, filename):
     data = {"response": content}
-    with open("response.json", "a") as jsonFile:
+    with open(f"{filename}.json", "a") as jsonFile:
+        json.dump(data, jsonFile)
+        jsonFile.write("\n")
+
+def translate(apiKey, content, temperature, filename):
+    openai.api_key = apiKey
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a consistent elementary teacher fluent in Sonoran Spanish.  Please translate the following questions and answers to Sonoran Spanish.  Always retain the original formatting."},
+            {"role": "user", "content": content}
+            ],
+        temperature=temperature
+        )
+    saveToJsonSpanish(completion.choices[0].message.content, filename)
+    return completion.choices[0].message.content
+
+def saveToJsonSpanish(content, filename):
+    data = {"response": content}
+    with open(f"{filename}_Spanish.json", "a") as jsonFile:
         json.dump(data, jsonFile)
         jsonFile.write("\n")
 
@@ -38,12 +54,37 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
     html.H1("Auto Deck"),
-    dbc.Input(id="apiKey", type="password", placeholder="Enter OpenAI API Key", className="mb-3"),
+    
+    dbc.Input(
+        id="apiKey", 
+        type="password", 
+        placeholder="Enter OpenAI API Key", 
+        className="mb-3"
+    ),
+
     dbc.Row([
         dbc.Col(dbc.Checkbox(id="showKeyCheckbox", className="form-check-input"), width="auto"),
-        dbc.Col(html.Label("Show API Key", htmlFor="showKeyCheckbox", className="form-check-label"))
+        dbc.Col(html.Label("Show API Key", htmlFor="showKeyCheckbox", className="form-check-label")),
     ], className="mb-3"),
-    dbc.Textarea(id="questionText", placeholder="Enter Content", className="mb-3"),
+
+    dbc.Input(
+        id="jsonFilename", 
+        type="text", 
+        placeholder="Enter JSON filename", 
+        className="mb-3"
+    ),
+
+    dbc.Textarea(
+        id="questionText", 
+        placeholder="Enter Content", 
+        className="mb-3"
+    ),
+
+    dbc.Row([
+        dbc.Col(dbc.Checkbox(id="translateCheckbox", className="form-check-input"), width="auto"),
+        dbc.Col(html.Label("Translate", htmlFor="translateCheckbox", className="form-check-label")),
+    ], className="mb-3"),
+
     dcc.Slider(
         id='tempSlider',
         min=0,
@@ -53,9 +94,12 @@ app.layout = dbc.Container([
         marks={i/10: str(i/10) for i in range(0, 11)},
         className="mb-3"
     ),
+
     html.Div(id='sliderOutput', className="mb-3"),
+
     dbc.Button("Enter", id="enterButton", color="primary", className="mb-3"),
-    html.Div(id="responseDiv")
+
+    html.Div(id="responseDiv"),
 ])
 
 @app.callback(
@@ -64,12 +108,18 @@ app.layout = dbc.Container([
     Input("enterButton", "n_clicks"),
     State("apiKey", "value"),
     State("questionText", "value"),
-    State("tempSlider", "value")
+    State("tempSlider", "value"),
+    State("jsonFilename", "value"),
+    State("translateCheckbox", "value")
 )
-def askQuestion(nClicks, apiKey, question, temp):
-    if nClicks is not None and apiKey:
-        response = getFlashcards(apiKey, question, temp)
-        return response, "Enter"
+def askQuestion(nClicks, apiKey, question, temp, filename, translateChecked):
+    if nClicks is not None and apiKey and filename and question:
+        response = getFlashcards(apiKey, question, temp, filename)
+        output = [html.P(response)]
+        if translateChecked:
+            translated_response = translate(apiKey, response, temp, filename)
+            output.append(html.P(translated_response))
+        return output, "Enter"
     return dash.no_update, "Enter"
 
 @app.callback(
